3d1ba0ad5bd6d6ea03cdc54eee0a3093f01afc6d
aybaneze <andrea270696@gmail.com>
2018-10-09 14:33:15 -0500

    Updates

---
+++ precache-manifest.5c33cbc9251b0ea3a888319962aa55f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45d975ffd74dd0f719d3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/runtime~main.45d975ff.js"
+  },
+  {
+    "revision": "f630526503d834fc84ee",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.f6305265.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "f630526503d834fc84ee",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/css/main.73157cf0.chunk.css"
+  },
+  {
+    "revision": "9903b0b4be3ef81b59ffba30cb47131c",
+    "url": "/lim-2018-01-lab-songs-frameworks/index.html"
+  }
+];
